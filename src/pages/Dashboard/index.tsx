15e278d4a9bbfaf94d27ee1c4e8140c1c8994449
Maurício Chaves <feira.nova@gmail.com>
2020-09-25 01:24:37 -0300

    finish dashboardt

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,48 +1,60 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepository, setNewRepository] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepository}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepository('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepository}
+          onChange={(e) => setNewRepository(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="#">
-          <img src="https://avatars1.githubusercontent.com/u/5477712?s=460&u=7088e1c7206e398d52f3eb42ae5e19bcfb7e723a&v=4"
-            alt="Owner Repository" />
-          <div>
-            <strong>mfchavesjr/go-barber-backend</strong>
-            <p>Descrição do Repositório</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#">
-          <img src="https://avatars1.githubusercontent.com/u/5477712?s=460&u=7088e1c7206e398d52f3eb42ae5e19bcfb7e723a&v=4"
-            alt="Owner Repository" />
-          <div>
-            <strong>mfchavesjr/go-barber-backend</strong>
-            <p>Descrição do Repositório</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#">
-          <img src="https://avatars1.githubusercontent.com/u/5477712?s=460&u=7088e1c7206e398d52f3eb42ae5e19bcfb7e723a&v=4"
-            alt="Owner Repository" />
-          <div>
-            <strong>mfchavesjr/go-barber-backend</strong>
-            <p>Descrição do Repositório</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="#">
+            <img src={repository.owner.avatar_url}
+              alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   )
